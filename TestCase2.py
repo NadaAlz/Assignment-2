963732caa9ee468dffbdd5294144349b7e22d632
NadaAlz <165168524+NadaAlz@users.noreply.github.com>
2024-03-27 21:46:17 +0400

    Update TestCase2.py

--- TestCase2.py
+++ TestCase2.py
@@ -4,7 +4,7 @@ from Exhibition import Exhibition
 from Exhibition import ExhibitionLocation
 from SpecialEvent import SpecialEvent
 from SpecialEvent import Purpose
-exhibition1 = Exhibition("UAE Heritage", "3 months", ExhibitionLocation.EH)
+exhibition1 = Exhibition("UAE Heritage", "3 hours", ExhibitionLocation.EH)
 exhibition1.display_new_exh()
 print("")
 special_event1 = SpecialEvent("Jazz Night", "4 hours", ExhibitionLocation.OS, Purpose.M)
