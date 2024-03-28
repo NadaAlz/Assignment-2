f4129d829096d64ff805e53ff591a0beb101b155
NadaAlz <165168524+NadaAlz@users.noreply.github.com>
2024-03-28 14:48:49 +0400

    Update Catalog.py

--- Catalog.py
+++ Catalog.py
@@ -4,16 +4,16 @@ class Catalog:
     def __init__(self):
         # Initialze catalog object with an empty catalog list
         # The attribute is protected using "_"
-        self._catalog = []  # Aggregation: Catalog contains a list of Artwork objects.
+        self.__catalog = []  # Aggregation: Catalog contains a list of Artwork objects.
 
     # Function to add the art object to the catalog list
     def add_art(self, art):
-        self._catalog.append(art)
+        self.__catalog.append(art)
         print(art.get_art_title(), "is successfully added to the museum")
 
     # Function to display each art object that is in the catalog
     def display_catalog(self):
-        for art in self._catalog:
+        for art in self.__catalog:
             print("Art Title:", art.get_art_title())
             print("Art Type:", art.get_art_type())
             print("Artist:", art.get_artist())
